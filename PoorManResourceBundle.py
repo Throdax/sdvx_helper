2c80abea9cdb024facd29da5e60cec6c960f4af5
Throdax <darkdeamon@gmail.com>
2025-05-14 21:59:17 +0200

    Externalized resource bundles into files. Updated musiclist.pkl

--- PoorManResourceBundle.py
+++ PoorManResourceBundle.py
@@ -1,93 +1,6 @@
+import os
 
-bundles = {
-    'en': {
-        'button.rescan': 'Colorize file list (heavy)',
-        'button.merge.pkl': 'Merge External PKL',
-        'button.send.pkl': 'Send PKL',
-        'button.resgister': 'Register Song',
-        
-        'text.difficulty': 'difficulty',
-        'text.registered': 'Registered',
-        'text.music_score': 'Music Score',
-        'text.songs': "Songs",
-        
-        'window.title': 'SDVX helper - OCR undetected song reporting tool',
-        
-        'message.coloring':'File list is being coloured according to OCR results. Please wait...',
-        'message.coloring.complete':'Colouring completed.',
-        'message.song.registered':'The song has not been registered. All score information will be registered.',
-        'message.song.not.registered':'This song is not registered in the song title database. Please select the song and then click "Register Song".',
-        'message.song.already.registered':'The song has already been registered.',
-        'message.hash.fixed':'The hash has been fixed.',
-        'message.non.result.images':'(Files that are not result images)',
-        'message.error.file.not.found':'Error! File not found',
-        'message.error.cannot.obtain':'Unable to obtain difficulty or hash value',
-        'message.error.no.title':'No song title entered',
-        'message.screenshot.saved':'Screenshot saved',
-        'message.on.detect':'Detect the song selection screen',
-        'message.obs.error':'Error!! Failed to connect to OBS.',
-        'message.version':'Your version is the latest',
-        
-        'log.song.registered':'Not registered. Register all score information.',
-        'log.song.already.registered':'The song itself has been registered.',
-        'log.hash.fixed':'Only fixes the hash.',
-        'log.filename.exists':'Skip as the file name already exists.',
-        'log.file.not.found':'File not found, skipping.',
-        'log.song.db':'Song DB',
-        'log.song.wiki':'Song information (based on bemaniwiki)',
-        
-        'webhook.number.added.scores':'Number of added scores',
-        'webhook.number.added.songs':'Number of songs',
-        'webhook.ocr.title': 'Title',
-        'webhook.ocr.hash.jacket': 'Jacket Hash',
-        'webhook.ocr.hash.info': 'Info Hash',
-        'webhook.ocr.difficulty': 'Difficulty'
-        
-    },
-    'ja': {
-        'button.rescan': 'ファイル一覧に色付け(重いです)',
-        'button.merge.pkl': '外部pklのマージ',
-        'button.send.pkl': 'pklを送信',
-        'button.resgister': '曲登録',
-        
-        'text.difficulty': '難易度',
-        'text.registered': '登録済',
-        'text.music_score': '譜面',
-        'text.songs': "曲",
-        
-        'window.title': 'SDVX helper - OCR未検出曲報告ツール',
-        
-        'message.coloring':'ファイル一覧をOCR結果に応じて色付け中。しばらくお待ちください。',
-        'message.coloring.complete':'色付けを完了しました。',
-        'message.song.registered':'曲が未登録。全譜面の情報を登録します。',
-        'message.song.not.registered':'曲名DBに登録されていません。曲を選択してから曲登録を押してもらえると喜びます。',
-        'message.song.already.registered':'曲自体は登録済み。',
-        'message.hash.fixed':'のhashを修正しました。',
-        'message.non.result.images':'(リザルト画像ではないファイル)',
-        'message.error.file.not.found':'error! ファイル見つかりません',
-        'message.error.cannot.obtain':'難易度またはハッシュ値が取得できません',
-        'message.error.no.title':'曲名が入力されていません',
-        'message.screenshot.saved':'スクリーンショットを保存しました',
-        'message.on.detect':'曲決定画面を検出',
-        'message.obs.error':'Error!! OBSとの接続に失敗しました。',
-        'message.version':'お使いのバージョンは最新です',
-        
-        'log.song.registered':'登録されていません。全譜面の情報を登録します。',
-        'log.song.already.registered':'曲自体の登録はされています。この譜面',
-        'log.hash.fixed':'のみhashを修正します。',
-        'log.filename.exists':'既に存在するファイル名なのでskip。',
-        'log.file.not.found':'ファイルが見つかりません。スキップします。',
-        'log.song.db':'認識用DB',
-        'log.song.wiki':'曲情報(bemaniwikiベース)',
-        
-        'webhook.number.added.scores':'追加した譜面数',
-        'webhook.number.added.songs':'曲数',
-        'webhook.ocr.title': '',
-        'webhook.ocr.hash.jacket': '',
-        'webhook.ocr.hash.info': '',
-        'webhook.ocr.difficulty': 'Difficulty'
-    }
-}
+bundles = {}
 
 
 class PoorManResourceBundle :
@@ -95,7 +8,30 @@ class PoorManResourceBundle :
     
     def __init__(self, locale:str='ja'):
         
+        self.loadBundles()
         self.bundle = bundles.get(locale.lower());
+        
+
+    def loadBundle(self, bundleFile, locale):
+        print(f'Loading bundle "{locale}" from file "{bundleFile}"...')
+        
+        with open('resources/i18n/' + bundleFile, 'r',encoding="utf-8") as f:
+            localeBundle = {}
+            for line in f.readlines():
+                key = line[:line.find("=")].strip()
+                value = line[line.find("=") + 1:].strip()
+                localeBundle[key] = value
+            
+            bundles[locale] = localeBundle
+            
+        print(f'Bundle "{locale}" loaded.')
+
+    def loadBundles(self):
+        bundleFiles = os.listdir('resources/i18n')
+        for bundleFile in bundleFiles :
+            locale = bundleFile[-13:-11]
+            self.loadBundle(bundleFile, locale)
+        
         
     def getText(self, key):
         if not self.bundle == None :
