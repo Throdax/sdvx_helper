63016d179623dec5b4aaf803608d5fa5380f4c15
Throdax <facristino@gmail.com>
2025-05-15 13:01:06 +0200

    Functionality improvements to PoorManResourceBundle
    Added get_text with placeholder args
    Added get_available_bundles
    Reformated to comply with Python standards

--- PoorManResourceBundle.py
+++ PoorManResourceBundle.py
@@ -8,36 +8,51 @@ class PoorManResourceBundle :
     
     def __init__(self, locale:str='ja'):
         
-        self.loadBundles()
+        self.load_bundles()
         self.bundle = bundles.get(locale.lower());
         
 
-    def loadBundle(self, bundleFile, locale):
-        print(f'Loading bundle "{locale}" from file "{bundleFile}"...')
+    def load_bundle(self, bundle_file, locale):
+        print(f'Loading bundle "{locale}" from file "{bundle_file}"...')
         
-        with open('resources/i18n/' + bundleFile, 'r',encoding="utf-8") as f:
-            localeBundle = {}
+        with open('resources/i18n/' + bundle_file, 'r',encoding="utf-8") as f:
+            locale_bundle = {}
             for line in f.readlines():
                 key = line[:line.find("=")].strip()
                 value = line[line.find("=") + 1:].strip()
-                localeBundle[key] = value
+                locale_bundle[key] = value
             
-            bundles[locale] = localeBundle
+            bundles[locale] = locale_bundle
             
         print(f'Bundle "{locale}" loaded.')
 
-    def loadBundles(self):
-        bundleFiles = os.listdir('resources/i18n')
-        for bundleFile in bundleFiles :
-            locale = bundleFile[-13:-11]
-            self.loadBundle(bundleFile, locale)
+    def load_bundles(self):
+        bundle_files = os.listdir('resources/i18n')
+        for bundle_file in bundle_files :
+            locale = bundle_file[-13:-11]
+            self.loadBundle(bundle_file, locale)
         
         
-    def getText(self, key):
-        if not self.bundle == None :
+    def get_text(self, key: str):
+        if self.bundle != None :
             return self.bundle.get(key)
         else :
-            raise 'Bundle not initialised'
+            raise RuntimeError('Bundle not initialized')
+
+    def get_text(self, key: str, *args):
+        message = getText(key)
+        for i, replacement in enumerate(args) :
+            token = '{'+i+'}'
+            if token in message :
+                message = message.replace(token,replacement)
+            else :
+                raise ValueError('More arguments supplied than available tokens on message')
+
+        return message
+
+    def get_available_bundles(self):
+        return list(bundles.keys())
+        
 
 
 if __name__ == '__main__':
