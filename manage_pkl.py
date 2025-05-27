db13ff7eafcdecf6430ff11717e534acc1e86bdf
Throdax <darkdeamon@gmail.com>
2025-05-27 20:48:21 +0200

    Fixed issues in gen_summary.py still dirived from the hash size change Added new setting "always_update_vf" Made it so it's impossible to register the "no jacket" has as part of played song.

--- manage_pkl.py
+++ manage_pkl.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # musiclist.pklに手動追加するやつなど
 import pickle
+import imagehash
+from PIL import Image
 
 def load():
     ret = None
@@ -16,10 +18,21 @@ if __name__ == '__main__':
     a = load()
 
     ##a['titles']['title'] = ['title', 'artist', 'bpm', 6,12,15,None]
-    a['titles']['弾幕信仰'] = ['弾幕信仰', '豚乙女×BEMANI Sound Team "PON"', '', 5,13,16,18]
-    a['titles']['Blue Fire'] = ['Blue Fire', 'REDALiCE feat. 野宮あゆみ', '', 4,8,12,16]
-    a['titles']['閉塞的フレーション'] = ['閉塞的フレーション', 'Pizuya\'s Cell VS BEMANI Sound Team "dj TAKA"', '', 4,12,15,18]
-    a['titles']['SUPER HEROINE!!'] = ['SUPER HEROINE!!', 'Amateras Records vs BEMANI Sound Team "TATSUYA" feat. miko', '', 3,12,15,17]
-    a['titles']['残像ニ繋ガレタ追憶ノHIDEAWAY'] = ['残像ニ繋ガレタ追憶ノHIDEAWAY', 'SOUND HOLIC Vs. BEMANI Sound Team "KE!JU" feat. Nana Takahashi', '', 3,11,14,17]
-
-    save(a)
+#    a['titles']['弾幕信仰'] = ['弾幕信仰', '豚乙女×BEMANI Sound Team "PON"', '', 5,13,16,18]
+#    a['titles']['Blue Fire'] = ['Blue Fire', 'REDALiCE feat. 野宮あゆみ', '', 4,8,12,16]
+#    a['titles']['閉塞的フレーション'] = ['閉塞的フレーション', 'Pizuya\'s Cell VS BEMANI Sound Team "dj TAKA"', '', 4,12,15,18]
+#    a['titles']['SUPER HEROINE!!'] = ['SUPER HEROINE!!', 'Amateras Records vs BEMANI Sound Team "TATSUYA" feat. miko', '', 3,12,15,17]
+    #a['titles']['Help me, ERINNNNNN!! #幻想郷ホロイズムver.'] = ['有頂天ビバーチェ', 'Last Note.', '266', 6,11,16,None]
+    #hash = a['jacket']['nov']['Help me, ERINNNNNN!! #幻想郷ホロイズムver.']
+    #print(hash)
+    #a['jacket']['adv']['Help me, ERINNNNNN!! #幻想郷ホロイズムver.'] = hash
+    #a['jacket']['exh']['Help me, ERINNNNNN!! #幻想郷ホロイズムver.'] = hash
+    ##a['jacket']['adv']['Help me, ERINNNNNN!! #幻想郷ホロイズムver.'] = ''
+    hash = a['jacket']['nov']['Starlight Express'] = ''
+    print(hash)
+#    
+    tmp = Image.open('resources/no_jacket.png')
+    hash_no_jacket = imagehash.average_hash(tmp,10)
+    print(hash_no_jacket)
+    
+#    save(a)
