59e77f91656c17d49fd1983cfec16301e352b454
Throdax <darkdeamon@gmail.com>
2025-06-19 00:47:39 +0200

    Trying a new way to no duplication VF screenshots. Updated musiclist.pkl with my latest play sessions

--- build.cmd
+++ build.cmd
@@ -1,27 +1,65 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
+
 set target_sdvx=sdvx_helper
 set target_ocr=ocr_reporter
 set target_manager=manage_score
 set pyin="%userprofile%\AppData\Roaming\Python\Python313\Scripts\pyinstaller.exe"
 
-%pyin% %target_sdvx%.pyw --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" --hidden-import=tkinter --hidden-import=tkinter.filedialog -y
-%pyin% %target_ocr%.py --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" --hidden-import=tkinter --hidden-import=tkinter.filedialog -y
-%pyin% %target_manager%.py --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" --hidden-import=tkinter --hidden-import=tkinter.filedialog -y
+set SDVX=Not Build
+set OCR=Not Build
+set MANAGER=Not Build
 
-REM xcopy /Y dist\*.exe to_bin\
-xcopy /Y dist\*.exe %target_sdvx%\
+if "%1" == "sdvx" set SDVX=Build
+if "%1" == "helper" set SDVX=Build
+if "%2" == "sdvx" set SDVX=Build
+if "%2" == "helper" set SDVX=Build
+if "%3" == "sdvx" set SDVX=Build
+if "%3" == "helper" set SDVX=Build
 
-REM del /F /Q %target_sdvx%\ocr_reporter.exe
-REM del /F /Q %target_sdvx%\manage_score.exe
+if "%1" == "ocr" set OCR=Build
+if "%2" == "ocr" set OCR=Build
+if "%3" == "ocr" set OCR=Build
+
+if "%1" == "manager" set MANAGER=Build
+if "%2" == "manager" set MANAGER=Build
+if "%3" == "manager" set MANAGER=Build
+
+if "%1" == "*" (
+	set SDVX=Build
+ 	set OCR=Build
+ 	set MANAGER=Build
+)
+if "%1" == "all" (
+	set SDVX=Build
+ 	set OCR=Build
+ 	set MANAGER=Build
+)
+if [%1] == [] (
+	set SDVX=Build
+ 	set OCR=Build
+ 	set MANAGER=Build
+)
+
+echo SDVX: %SDVX%
+echo OCR: %OCR%
+echo MANAGER: %MANAGER%
+
+del /F /Q dist\*.exe
+
+if "%SDVX%" == "Build" %pyin% %target_sdvx%.pyw --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" --hidden-import=tkinter --hidden-import=tkinter.filedialog -y
+if "%OCR%" == "Build" %pyin% %target_ocr%.py --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" --hidden-import=tkinter --hidden-import=tkinter.filedialog -y
+if "%MANAGER%" == "Build" %pyin% %target_manager%.py --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" --hidden-import=tkinter --hidden-import=tkinter.filedialog -y
+
+xcopy /Y dist\sdvx_helper.exe %target_sdvx%\
+
 del /F /Q %target_sdvx%\out\rival*.pkl
 
-REM xcopy /E /I /Y resources to_bin\
 xcopy /E /I /Y resources %target_sdvx%\resources
 
 del /F /Q out\*.xml
 del /F /Q out\*.pkl
 
-REM xcopy /E /I /Y out to_bin\
 xcopy /E /I /Y out %target_sdvx%\out
 
 copy version.txt %target_sdvx%\
